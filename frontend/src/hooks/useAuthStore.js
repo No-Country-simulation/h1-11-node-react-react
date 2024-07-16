27bff9f449dfbedbb6d2be9e5bc0fcd9bfe4c046
CarlosV319 <carlos.alberto_88@hotmail.com>
2024-07-15 23:39:52 -0300

    backConnection

--- frontend/src/hooks/useAuthStore.js
+++ frontend/src/hooks/useAuthStore.js
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux'
 // import Swal from 'sweetalert2'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 // import { pageApi } from '../api/PageApi'
-import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store/auth/authSlice'
+import {  onChecking } from '../store/auth/authSlice'
 import axios from 'axios'
 
 export const useAuthStore = () => {
@@ -11,31 +11,35 @@ export const useAuthStore = () => {
   const dispatch = useDispatch()
 
  
-  const navigateTo = useNavigate()
+  // const navigateTo = useNavigate()
+
 
   const startRegister = async User => {
     dispatch(onChecking())
     try {
       const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, {
-        ...User
+     
+        ...User,
       })
-      localStorage.setItem('token', data.token)
-      localStorage.setItem('token-init-date', new Date().getTime())
-      dispatch(onLogin({ name: data.user.name, uid: data.user.id }))
+      console.log(User)
+      console.log(data)
+      // localStorage.setItem('token', data.token)
+      // localStorage.setItem('token-init-date', new Date().getTime())
+      // dispatch(onLogin({ name: data.user.name, uid: data.user.id }))
       console.log('Usuario creado correctamente', 'success')
       // Swal.fire('Usuario correctamente registrado!')
-      navigateTo(`/dashboard`)
+      // navigateTo(`/dashboard`)
     } catch (error) {
       // Swal.fire({
       //   icon: 'error',
       //   title: 'Oops...',
       //   text: error.response.data?.details
       // })
-      console.log('Error al crear usuario', error.response.data?.details, 'error')
-      dispatch(onLogout(error.response.data?.msg || 'add valid email or password'))
-      setTimeout(() => {
-        dispatch(clearErrorMessage())
-      }, 10)
+      console.log('Error al crear usuario', error.message)
+      // dispatch(onLogout(error.response.data?.msg || 'add valid email or password'))
+      // setTimeout(() => {
+      //   dispatch(clearErrorMessage())
+      // }, 10)
     }
   }
 
