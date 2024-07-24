474116efde619226d72d0762708cd5c44dfc23a3
ricardo <rblazquez111@gmail.com>
2024-07-24 16:01:06 -0300

    Front/feat(home): add page home and Links

--- frontend/src/router/AuthRoutes.jsx
+++ frontend/src/router/AuthRoutes.jsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import HomePage from "../pages/Home/HomePage";
+import ProfilePage from "../pages/Profile/ProfilePage";
+import AppointmentsPage from "../pages/Appointments/AppointmentPage";
+import RecipesPage from "../pages/Recipes/RecipesPage";
 
 export const AuthRoutes = () => {
   return (
@@ -11,6 +14,9 @@ export const AuthRoutes = () => {
       <Route path="/*" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/appointments" element={<AppointmentsPage />} />
+      <Route path="/recipes" element={<RecipesPage />} />
     </Routes>
   );
 };
