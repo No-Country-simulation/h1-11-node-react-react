209d4a3ebc3644c9c21b47dd0559355ca2251014
spimentel1201 <spimentelm1201@gmail.com>
2024-07-31 11:30:49 -0500

    fix filters and add search user types

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PaginationDto } from 'src/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserWithTokenResponseDto } from 'src/auth/dto/response-create-patient';
 
 @ApiTags('Users')
@@ -16,6 +16,59 @@ export class UsersController {
   @Get()
   findAll(@Query() paginationDto : PaginationDto) {
     return this.usersService.findAll(paginationDto);
+  }
+
+  @ApiOperation({
+    summary: 'Búsqueda de médicos',
+    description:
+      'Médicos.',
+  })
+  @ApiResponse({ status: 201, description: 'Filtro de médicos' })
+  @ApiResponse({ status: 400, description: 'BadRequest' })
+  @ApiResponse({ status: 403, description: 'Forbidden, Token' })
+  @ApiBearerAuth()
+  @ApiQuery({
+    name: 'name',
+    type: String,
+    required: false,
+    description: 'Filter by doctor name' })
+  @ApiQuery({
+    name: 'lastName',
+    type: String,
+    required: false,
+    description: 'Filter by doctor lastname' })
+  @Get('search-doctors')
+  findDoctors(@Query('name') name?: string, @Query('name') lastName?: string) {
+    return this.usersService.findDoctorsByName(lastName,name);
+  }
+
+  @ApiOperation({
+    summary: 'Búsqueda de pacientes',
+    description:
+      'Pacientes.',
+  })
+  @ApiResponse({ status: 201, description: 'Filtro de pacientes' })
+  @ApiResponse({ status: 400, description: 'BadRequest' })
+  @ApiResponse({ status: 403, description: 'Forbidden, Token' })
+  @ApiBearerAuth()
+  @ApiQuery({
+    name: 'name',
+    type: String,
+    required: false,
+    description: 'Filter by doctor name' })
+  @ApiQuery({
+    name: 'lastName',
+    type: String,
+    required: false,
+    description: 'Filter by doctor lastname' })
+  @ApiQuery({
+    name: 'dni',
+    type: String,
+    required: false,
+    description: 'Filter by doctor DNI number' })
+  @Get('search-patients')
+  findPatients(@Query('name') name?: string, @Query('name') lastName?: string, @Query('dni') dni?: string) {
+    return this.usersService.findPatientByName(lastName,name,dni);
   }
 
   @ApiResponse({ status: 200, description: 'get user by id', type: UserWithTokenResponseDto })
