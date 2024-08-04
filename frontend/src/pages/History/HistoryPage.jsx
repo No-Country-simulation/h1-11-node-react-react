7f631ce5433c01a8d47ff8f1240e760689f8e622
ricardo <rblazquez111@gmail.com>
2024-08-04 20:29:56 -0300

    front/home: add container and links

---
+++ frontend/src/pages/History/HistoryPage.jsx
@@ -0,0 +1,9 @@
+export default function HistoryPage() {
+    return (
+        <div>
+            <h1>Aca va el historial medico</h1>
+            <h1>lista de alergias y enfermedades</h1>
+            <h1>lista de operaciones y detalles</h1>
+        </div>
+    );
+}
